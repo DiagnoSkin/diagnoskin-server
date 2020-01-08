cb1d4e114370ae0e6dddd0126bd13afc3d69628b
Bartlomiej Szal <bartlomiej.szal@gmail.com>
2020-01-08 20:17:12 +0100

    Make test endpoint not use the ai model

--- resources/hello.py
+++ resources/hello.py
@@ -4,21 +4,16 @@ import base64
 from io import BytesIO
 from PIL import Image
 import numpy as np
+
 class Hello(Resource):
     def post(self):
-        if 'Authentication' in request.headers:
-            token = request.headers['Authentication']
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
             parser = reqparse.RequestParser()
             parser.add_argument('image', required=True)
             data = parser.parse_args()
-
             image = Image.open(BytesIO(base64.b64decode(data.image)))
             image_numpy = np.array(image)
             return { 'message': f"data: {image_numpy}" }
         else:
             abort(400, error='Cannot find Authentication header')
-
-    def get(self):
-        from main import model
-        return { 'message': f"{model.layers}" }
-
